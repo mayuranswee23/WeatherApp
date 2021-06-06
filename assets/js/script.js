4c94b0c5e43c9e729c4e491b0f284d3f0325c8b3
Mayuran <mayuranswee23@gmail.com>
2021-06-06 18:50:22 -0400

    fetch function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,42 @@
-var key = '27da8b73e6fb1d73f946f7ce7ad0c662'
-var url = `api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${key}`
-var city = document.getElementById("cityName").value
+var key = '2d551bf0a67809e27c38c5bc0c75da65'
+// var url = `https:api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${key}`
+
 // when a user inputs a city name and press the button, the city name and weather will appear 
 
 var getCityName = document.querySelector('form').addEventListener('submit', function(event){
     event.preventDefault();
     var cityName = document.getElementById("cityName").value
-    console.log(cityName)
+
+    if(!cityName) {
+        // Show error;
+        window.alert('Please enter a city name!')
+        return;
+    }
+    // console.log(cityName)
+    var url = `https:api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${key}`
+    getWeather(url)
 })
 
-var getData = function (){
-    fetch(`https:api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}`)
-    .then((response)=>response.json())
-    .then((data)=>populatePage(data))
-    console.log(data)
+
+
+
+function getWeather (weatherApiUrl){
+fetch(weatherApiUrl)
+.then(response => response.json())
+.then(data => populatePage(data));
 }
 
-getData(cityName)
 
+// var getData = function (){
+//     var city = document.getElementById("cityName").value
+//     fetch(`https:api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}`)
+//     .then((response)=>response.json())
+//     .then(()=>populatePage())
+//     console.log(data)
+// }
+
+// getData(cityName)
+
+var populatePage = function (data){
+    console.log(data)
+}
