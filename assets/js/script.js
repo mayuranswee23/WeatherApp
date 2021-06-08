c80d2825de3367418b39ec0690ba6c65bef7c3db
Mayuran <mayuranswee23@gmail.com>
2021-06-08 02:33:15 -0400

    added weather conditions for current day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var key = '2d551bf0a67809e27c38c5bc0c75da65'
+var key = 'afa013543942d5e89b71cb83d24b61a7'
 // var url = `https:api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${key}`
 
 // when a user inputs a city name and press the button, the city name and weather will appear 
@@ -18,25 +18,123 @@ var getCityName = document.querySelector('form').addEventListener('submit', func
 })
 
 
-
-
 function getWeather (weatherApiUrl){
 fetch(weatherApiUrl)
 .then(response => response.json())
 .then(data => populatePage(data));
 }
 
-
-// var getData = function (){
-//     var city = document.getElementById("cityName").value
-//     fetch(`https:api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}`)
-//     .then((response)=>response.json())
-//     .then(()=>populatePage())
-//     console.log(data)
-// }
-
-// getData(cityName)
+//Select City Name
 
 var populatePage = function (data){
     console.log(data)
+    var city = document.getElementById("city")
+    var selectedCity = data.city.name
+    city.innerHTML = selectedCity
+
+    var date = document.getElementById("currentDate")
+    var selectedDate = data.list[0].dt_txt
+    date.innerHTML = selectedDate
+    
+    // date.appendChild(selectedDate)
+    // date.innerHTML = selectedDate
+    // date.appendChild(dateText)
+    // var displayDate = document.createElement('span')
+    // displayDate.className = "display"
+    // displayDate.textContent = selectedDate
+    
+    
+    // var temperature = document.getElementById("currentTemperature")
+    // var selectedTemp = data.list[0].main.temp
+    // var tempCelsius = selectedTemp - 272.15
+    // Math.floor(tempCelsius)
+    // var displayTemp = document.createElement('p')
+    // displayTemp.textContent = tempCelsius
+    // temperature.appendChild(displayTemp)
+
+    var temperature = document.getElementById("currentTemperature")
+    var selectedTemp = data.list[0].main.temp
+    var tempCelsius = selectedTemp - 272.15
+    var tempFloor = Math.floor(tempCelsius)
+    temperature.innerText = tempFloor
+    var celsius = document.createTextNode(" °C")
+    temperature.appendChild (celsius)
+
+    var wind = document.getElementById("currentWindSpeed")
+    var selectedWind = data.list[0].wind.speed
+    var windSpeed = selectedWind * 3.6
+    var roundedSpeed = Math.round(windSpeed*10)/10
+    wind.innerText = roundedSpeed
+    var kmH = document.createTextNode(" km/h")
+    wind.appendChild (kmH)
+
+    var icon = document.getElementById("icon")
+    var iconSelected = data.list[0].weather[0].icon
+    var iconUrl = 'http://openweathermap.org/img/wn/'+ iconSelected +'@2x.png'
+    var image = new Image ();
+    image.src = iconUrl;
+    icon.appendChild(image)
+
+    //obtaining UV index from another API
+    var lat = data.city.coord.lat
+    var lon = data.city.coord.lon
+    var uvUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${key}`
+    console.log(uvUrl)
+
+    fetch(uvUrl)
+    .then(response => response.json())
+    .then(data => populateUv(data));
+
+
 }
+
+function populateUv (data){
+    console.log(data)
+    var uvIndex = document.getElementById("uvIndex")
+    var selectedUv = data.current.uvi
+    var uvSpan = document.createElement('span')
+    uvSpan.textContent = selectedUv
+
+    if (selectedUv <= 2){
+    uvSpan.setAttribute ('id', 'uvReadingGreen')
+    }
+    if (selectedUv >2 && selectedUv <=5 ){
+     uvSpan.setAttribute ('id', 'uvReadingYellow') 
+    }
+    if (selectedUv >5 && selectedUv <=7 ){
+        uvSpan.setAttribute ('id', 'uvReadingOrange') 
+     }
+    if (selectedUv >7 && selectedUv <=10 ){
+        uvSpan.setAttribute ('id', 'uvReadingRed') 
+    }
+    if (selectedUv >10){
+        uvSpan.setAttribute ('id', 'uvReadingPurple')
+        }
+        
+    uvIndex.appendChild(uvSpan)
+}
+
+
+
+
+// var uvUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${key}`
+
+// function getUv(cityName){
+//     var uvIndex = document.getElementById("uvIndex")
+//     var lat = data.city.coord.lat
+//     var lon = data.city.coord.lon
+//     console.log(uvUrl)
+// }
+
+//     fetch(uvUrl)
+//     .then(response => response.json())
+//     .then(data => populateUv(data));
+
+
+
+    // function getData(city){
+    //     var url = `https:api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}`;
+    //     fetch(url)
+    //   .then((response) => response.json())
+    //   .then((shark) => populatePage(shark));
+    // }
