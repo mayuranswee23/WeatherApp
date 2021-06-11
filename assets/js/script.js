da3550cd2eab9957d8b16c0cbea364a91495b680
Mayuran <mayuranswee23@gmail.com>
2021-06-11 00:31:06 -0400

    added five day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,10 +76,7 @@ var populatePage = function (data){
 
     fetch(uvUrl)
     .then(response => response.json())
-    .then(data => populateData(data));
-    
-
-
+    .then(data => populateData(data)); 
 }
 
 function populateData (data){
@@ -106,7 +103,7 @@ function populateData (data){
         }
      uvIndex.appendChild(uvSpan)
 
-    
+    //day One 
      var dayOne = document.getElementById("dayOneDate")
      var today = new Date();
      var date =  (today.getMonth()+1) + '/' + (today.getDate()+1) + '/' +  today.getFullYear() ;
@@ -142,17 +139,164 @@ function populateData (data){
     var humidityNotation = document.createTextNode (" %")
     dayOneHumidity.appendChild(humidityNotation)
 
-    for (var i =1; i<6; i++){
-        var Temp = data.daily[i].temp.day
-        var Wind = data.daily[i].wind_speed
-        var Humidity = data.daily[i].humidity
-        var Icon = data.daily[i].weather[0].icon
-        var date =  (today.getMonth()+[1]) + '/' + (today.getDate()+[i]) + '/' +  today.getFullYear() ;
-        console.log(Temp)
-        console.log(Wind)
-        console.log(Humidity)
-        console.log(date)
-    }
+    //day Two
+
+    var dayTwo = document.getElementById("dayTwoDate")
+    var tomorrow = new Date();
+    var nextDate =  (tomorrow.getMonth()+1) + '/' + (tomorrow.getDate()+2) + '/' +  tomorrow.getFullYear() ;
+   dayTwo.innerHTML = nextDate
+  
+   var dayTwoIcon = document.getElementById("dayTwoIcon")
+   var dayTwoSelectedIcon = data.daily[2].weather[0].icon
+   var dayTwoIconUrl = 'http://openweathermap.org/img/wn/'+ dayTwoSelectedIcon +'@2x.png'
+   var dayTwoimage = new Image ();
+   dayTwoimage.src = dayTwoIconUrl;
+   dayTwoIcon.appendChild(dayTwoimage)
+
+   var dayTwoTemp = document.getElementById("dayTwoTemp")
+   var selectedDayTwoTemp = data.daily[2].temp.day
+   var dayTwoCelsius = selectedDayTwoTemp
+   var tempDayTwoFloor = Math.floor(dayTwoCelsius)
+   dayTwoTemp.innerText = tempDayTwoFloor
+   var TwoCelsius = document.createTextNode(" °C")
+   dayTwoTemp.appendChild (TwoCelsius)
+
+   var dayTwoWind = document.getElementById("dayTwoWind")
+   var dayTwoSelectedWind = data.daily[2].wind_speed
+   var dayTwoWindSpeed = dayTwoSelectedWind * 3.6
+   var dayTwoRoundedSpeed = Math.round(dayTwoWindSpeed*10)/10
+   dayTwoWind.innerText = dayTwoRoundedSpeed
+   var dayTwoKmh = document.createTextNode(" km/h")
+   dayTwoWind.appendChild (dayTwoKmh)
+
+   var dayTwoHumidity = document.getElementById("dayTwoHumidity")
+   var selectedDayTwoHumidity = data.daily[2].humidity
+   dayTwoHumidity.innerHTML = selectedDayTwoHumidity
+   var twoHumidityNotation = document.createTextNode (" %")
+   dayTwoHumidity.appendChild(twoHumidityNotation)
+
+// day three
+
+var dayThree = document.getElementById("dayThreeDate")
+    var dayAfter = new Date();
+    var thirdDate =  (dayAfter.getMonth()+1) + '/' + (dayAfter.getDate()+3) + '/' +  dayAfter.getFullYear() ;
+   dayThree.innerHTML = thirdDate
+  
+   var dayThreeIcon = document.getElementById("dayThreeIcon")
+   var dayThreeSelectedIcon = data.daily[3].weather[0].icon
+   var dayThreeIconUrl = 'http://openweathermap.org/img/wn/'+ dayThreeSelectedIcon +'@2x.png'
+   var dayThreeimage = new Image ();
+   dayThreeimage.src = dayThreeIconUrl;
+   dayThreeIcon.appendChild(dayThreeimage)
+
+   var dayThreeTemp = document.getElementById("dayThreeTemp")
+   var selectedDayThreeTemp = data.daily[3].temp.day
+   var dayThreeCelsius = selectedDayThreeTemp
+   var tempDayThreeFloor = Math.floor(dayThreeCelsius)
+   dayThreeTemp.innerText = tempDayThreeFloor
+   var ThreeCelsius = document.createTextNode(" °C")
+   dayThreeTemp.appendChild (ThreeCelsius)
+
+   var dayThreeWind = document.getElementById("dayThreeWind")
+   var dayThreeSelectedWind = data.daily[3].wind_speed
+   var dayThreeWindSpeed = dayThreeSelectedWind * 3.6
+   var dayThreeRoundedSpeed = Math.round(dayThreeWindSpeed*10)/10
+   dayThreeWind.innerText = dayThreeRoundedSpeed
+   var dayThreeKmh = document.createTextNode(" km/h")
+   dayThreeWind.appendChild (dayThreeKmh)
+
+   var dayThreeHumidity = document.getElementById("dayThreeHumidity")
+   var selectedDayThreeHumidity = data.daily[3].humidity
+   dayThreeHumidity.innerHTML = selectedDayThreeHumidity
+   var threeHumidityNotation = document.createTextNode (" %")
+   dayThreeHumidity.appendChild(threeHumidityNotation)
+
+
+   //dayfour 
+   var dayFour = document.getElementById("dayFourDate")
+   var dayAfterTom = new Date();
+   var fourDate =  (dayAfterTom.getMonth()+1) + '/' + (dayAfterTom.getDate()+4) + '/' +  dayAfterTom.getFullYear() ;
+  dayFour.innerHTML = fourDate
+  // dayOne.appendChild(date)
+
+  var dayFourIcon = document.getElementById("dayFourIcon")
+  var dayFourSelectedIcon = data.daily[4].weather[0].icon
+  var dayFourIconUrl = 'http://openweathermap.org/img/wn/'+ dayFourSelectedIcon +'@2x.png'
+  var dayFourimage = new Image ();
+  dayFourimage.src = dayFourIconUrl;
+  dayFourIcon.appendChild(dayFourimage)
+
+  var dayFourTemp = document.getElementById("dayFourTemp")
+  var selectedDayFourTemp = data.daily[4].temp.day
+  var dayFourCelsius = selectedDayFourTemp
+  var tempDayFourFloor = Math.floor(dayFourCelsius)
+  dayFourTemp.innerText = tempDayFourFloor
+  var FourCelsius = document.createTextNode(" °C")
+  dayFourTemp.appendChild (FourCelsius)
+
+  var dayFourWind = document.getElementById("dayFourWind")
+  var dayFourSelectedWind = data.daily[4].wind_speed
+  var dayFourWindSpeed = dayFourSelectedWind * 3.6
+  var dayFourRoundedSpeed = Math.round(dayFourWindSpeed*10)/10
+  dayFourWind.innerText = dayFourRoundedSpeed
+  var dayFourKmh = document.createTextNode(" km/h")
+  dayFourWind.appendChild (dayFourKmh)
+
+  var dayFourHumidity = document.getElementById("dayFourHumidity")
+  var selectedDayFourHumidity = data.daily[4].humidity
+  dayFourHumidity.innerHTML = selectedDayFourHumidity
+  var fourHumidityNotation = document.createTextNode (" %")
+  dayFourHumidity.appendChild(fourHumidityNotation)
+
+
+  //day five
+  var dayFifth = document.getElementById("dayFiveDate")
+   var dayFive = new Date();
+   var fiveDate =  (dayFive.getMonth()+1) + '/' + (dayFive.getDate()+5) + '/' +  dayFive.getFullYear() ;
+  dayFifth.innerHTML = fiveDate
+  // dayOne.appendChild(date)
+
+  var dayFiveIcon = document.getElementById("dayFiveIcon")
+  var dayFiveSelectedIcon = data.daily[5].weather[0].icon
+  var dayFiveIconUrl = 'http://openweathermap.org/img/wn/'+ dayFiveSelectedIcon +'@2x.png'
+  var dayFiveimage = new Image ();
+  dayFiveimage.src = dayFiveIconUrl;
+  dayFiveIcon.appendChild(dayFiveimage)
+
+  var dayFiveTemp = document.getElementById("dayFiveTemp")
+  var selectedDayFiveTemp = data.daily[5].temp.day
+  var dayFiveCelsius = selectedDayFiveTemp
+  var tempDayFiveFloor = Math.floor(dayFiveCelsius)
+  dayFiveTemp.innerText = tempDayFiveFloor
+  var FiveCelsius = document.createTextNode(" °C")
+  dayFiveTemp.appendChild (FiveCelsius)
+
+  var dayFiveWind = document.getElementById("dayFiveWind")
+  var dayFiveSelectedWind = data.daily[5].wind_speed
+  var dayFiveWindSpeed = dayFiveSelectedWind * 3.6
+  var dayFiveRoundedSpeed = Math.round(dayFiveWindSpeed*10)/10
+  dayFiveWind.innerText = dayFiveRoundedSpeed
+  var dayFiveKmh = document.createTextNode(" km/h")
+  dayFiveWind.appendChild (dayFiveKmh)
+
+  var dayFiveHumidity = document.getElementById("dayFiveHumidity")
+  var selectedDayFiveHumidity = data.daily[5].humidity
+  dayFiveHumidity.innerHTML = selectedDayFiveHumidity
+  var fiveHumidityNotation = document.createTextNode (" %")
+  dayFiveHumidity.appendChild(fiveHumidityNotation)
+
+
+    // for (var i =1; i<6; i++){
+    //     var Temp = data.daily[i].temp.day
+    //     var Wind = data.daily[i].wind_speed
+    //     var Humidity = data.daily[i].humidity
+    //     var Icon = data.daily[i].weather[0].icon
+    //     var date =  (today.getMonth()+[1]) + '/' + (today.getDate()+[i]) + '/' +  today.getFullYear() ;
+    //     console.log(Temp)
+    //     console.log(Wind)
+    //     console.log(Humidity)
+    //     console.log(date)
+    // }
 }
 
 
